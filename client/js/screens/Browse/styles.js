f1e372837cab32929455143ce08e73e6951910c3
Yen Yin Hing <yenyinf287@gmail.com>
2019-09-04 20:11:57 -0700

    amend styles

--- client/js/screens/Browse/styles.js
+++ client/js/screens/Browse/styles.js
@@ -1,6 +1,21 @@
 import {StyleSheet} from 'react-native';
 
+// let {width, height} = Dimensions.get('window');
+
 const styles = StyleSheet.create({
+  //   text: {
+  //     fontSize: 18,
+  //     marginHorizontal: 5,
+  //   },
+  //   browseImage: {
+  //     height: '100%',
+  //     width: '100%',
+  //   },
+  //   imageContainer: {
+  //     height: '60%',
+  //     margin: 20,
+  //     paddingBottom: 10,
+  //   },
   text: {
     fontSize: 18,
     marginHorizontal: 10,
@@ -8,8 +23,6 @@ const styles = StyleSheet.create({
   browseImage: {
     height: 180,
     width: '94%',
-    alignItems: 'center',
-    justifyContent: 'center',
     marginHorizontal: 10,
     marginTop: 10,
   },
