e456f1c1532430c5c6c02c3895bb2d69e8869ce8
Yen Yin Hing <yenyinf287@gmail.com>
2019-09-05 19:41:57 -0700

    setup test

---
+++ client/js/config/styles.js
@@ -0,0 +1,11 @@
+export const colors = {
+  //   MediumGrey: '#999999',
+  //   LightGrey: '#e6e6e6',
+  //   Blue: '#8797D6',
+  //   Purple: '#9963ea',
+  //   Red: '#cf392a',
+};
+
+export const fonts = {
+  baseFont: 'Lato',
+};
