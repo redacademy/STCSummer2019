bee6e0f4e849bc39f004c194c28257b02d3946ab
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-09-06 15:41:45 -0700

    made changes for merge conflict

--- client/js/screens/Browse/Browse.js
+++ client/js/screens/Browse/Browse.js
@@ -4,58 +4,56 @@ import styles from './styles';
 
 const Browse = ({navigation}) => {
   return (
-    <View>
-      <ScrollView>
-        <View>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('items');
-            }}>
-            <Image
-              source={require('../../assets/images/clothing.png')}
-              style={styles.browseImage}
-            />
-            <Text style={styles.text}>Clothing</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('items');
-            }}>
-            <Image
-              source={require('../../assets/images/shoes.png')}
-              style={styles.browseImage}
-            />
-            <Text style={styles.text}> Shoes </Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('items');
-            }}>
-            <Image
-              source={require('../../assets/images/bags.png')}
-              style={styles.browseImage}
-            />
-            <Text style={styles.text}>Bags</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('items');
-            }}>
-            <Image
-              source={require('../../assets/images/accessories.png')}
-              style={styles.browseImage}
-            />
-            <Text style={styles.text}>Accessories</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </View>
+    <ScrollView>
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('items');
+          }}>
+          <Image
+            source={require('../../assets/images/clothing.png')}
+            style={styles.browseImage}
+          />
+          <Text style={styles.text}>Clothing</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('items');
+          }}>
+          <Image
+            source={require('../../assets/images/shoes.png')}
+            style={styles.browseImage}
+          />
+          <Text style={styles.text}> Shoes </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('items');
+          }}>
+          <Image
+            source={require('../../assets/images/bags.png')}
+            style={styles.browseImage}
+          />
+          <Text style={styles.text}>Bags</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('items');
+          }}>
+          <Image
+            source={require('../../assets/images/accessories.png')}
+            style={styles.browseImage}
+          />
+          <Text style={styles.text}>Accessories</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
