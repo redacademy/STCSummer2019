1b8a3a3bd3ac8aa58b80971864cff19e3be07e4d
Yen Yin Hing <yenyinf287@gmail.com>
2019-09-07 14:32:54 -0700

    implement global color styles

--- client/js/screens/Browse/styles.js
+++ client/js/screens/Browse/styles.js
@@ -1,10 +1,11 @@
 import {StyleSheet} from 'react-native';
-import {fonts} from '../../config/styles';
+import {fonts, colors} from '../../config/styles';
 const styles = StyleSheet.create({
   text: {
     fontSize: 18,
     marginHorizontal: 10,
     fontFamily: fonts.baseFont,
+    color: colors.Black,
   },
   browseImage: {
     height: 180,
