70ce7a9b13c78cb18247236a98abbab7a4dfa078
Yen Yin Hing <yenyinf287@gmail.com>
2019-09-10 15:15:50 -0700

    add provider

--- client/js/App.js
+++ client/js/App.js
@@ -1,15 +1,19 @@
-
-
 import React from 'react';
-import { ApolloProvider } from '@apollo/react-hooks'
-import client from './config/api'
+import {ApolloProvider} from '@apollo/react-hooks';
+import client from './config/api';
 import RootStackNavigator from '../js/navigation/RootStackNavigator';
-import { FaveStoresProvider } from './context/FaveStoresContext'
+import {FaveStoresProvider} from './context/FaveStoresContext';
+import {FaveBrandsProvider} from './context/FaveBrandsContext';
+import {FaveItemsProvider} from './context/FaveItemsContext';
 const App = () => {
   return (
     <ApolloProvider client={client}>
       <FaveStoresProvider>
-        <RootStackNavigator />
+        <FaveBrandsProvider>
+          <FaveItemsProvider>
+            <RootStackNavigator />
+          </FaveItemsProvider>
+        </FaveBrandsProvider>
       </FaveStoresProvider>
     </ApolloProvider>
   );
