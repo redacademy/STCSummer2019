f440c007df3f65d9756ac36c427a2e641f6214b3
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-09-11 12:04:04 -0700

    add store hours

--- client/js/screens/Store/Store.js
+++ client/js/screens/Store/Store.js
@@ -1,32 +1,43 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import FaveStoresContext from '../../context/FaveStoresContext';
+import ImageCarousel from '../../components/ImageCarousel';
+import styles from './styles';
 
-const Store = ({ store }) => {
+const Store = ({store}) => {
+  const weekdayHours = store.hours.split(', ')[0];
+  const sundayHours = store.hours.split(', ')[1];
   return (
     <FaveStoresContext.Consumer>
-      {
-        ({ faveStoreIds, removeFaveStore, createFaveStore }) => {
-          return (
+      {({faveStoreIds, removeFaveStore, createFaveStore}) => {
+        return (
+          <View>
+            <ImageCarousel images={store.images} id={store.id} />
             <View>
-              <Text>Store Page</Text>
               <Text>{store.title}</Text>
               <TouchableOpacity
-                onPress={() => removeFaveStore(store.id)}
-                activeOpacity={0.5}
-              >
-                <Text >Remove From Faves</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => createFaveStore(store.id)}
-                activeOpacity={0.5}
-              >
-                <Text>Add To Faves</Text>
+                style={styles.mapLinkContainer}
+                onPress={() => {
+                  Linking.openURL(
+                    'http://maps.apple.com/?ll=37.484847,-122.148386',
+                  );
+                }}>
+                <Text style={styles.mapText}>Map View</Text>
               </TouchableOpacity>
             </View>
-          )
-        }
-      }
+            {/* <View style={styles.dayHoursContainer}> */}
+            <View style={styles.daysContainer}>
+              <Text>Mon-Sat</Text>
+              <Text>Sun</Text>
+            </View>
+            <View style={styles.hoursContainer}>
+              <Text>{weekdayHours}</Text>
+              <Text>{sundayHours} </Text>
+            </View>
+            {/* </View> */}
+          </View>
+        );
+      }}
     </FaveStoresContext.Consumer>
   );
 };
---
+++ client/js/screens/Store/styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  //map link
+  mapLinkContainer: {
+    width: '100%',
+    justifyContent: 'flex-start',
+    marginTop: '3%',
+  },
+  mapText: {
+    fontSize: 14,
+    textDecorationLine: 'underline',
+    fontFamily: 'Lato-bold',
+  },
+  dayHoursContainer: {
+    // flexDirection: 'row',
+    // width: '60%',
+  },
+});
+
+export default styles;
