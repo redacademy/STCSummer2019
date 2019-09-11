28778ae7ca7d3f5887ccd816bff89467fc885717
verasunyy <verasun@live.com>
2019-09-10 17:35:11 -0700

    add sale stores

--- client/js/components/StoreCard/StoreCard.js
+++ client/js/components/StoreCard/StoreCard.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import {Image, Text, View, TouchableOpacity} from 'react-native';
+import { Image, Text, View, TouchableOpacity } from 'react-native';
 import FaveStoresContext from '../../context/FaveStoresContext';
 import styles from './styles';
 
-const StoreCard = ({store, navigation}) => (
-  <TouchableOpacity onPress={() => navigation.navigate('store', {store})}>
+const StoreCard = ({ store, navigation }) => (
+  <TouchableOpacity onPress={() => navigation.navigate('store', { store })}>
     <View style={styles.storeCardContainer}>
       <View style={styles.imageContainer}>
         <Image
           //   source={{uri: `${store.images}`}}
-          source={{uri: 'https://via.placeholder.com/150'}}
+          source={{ uri: 'https://via.placeholder.com/150' }}
           style={styles.image}
           resizeMode="contain"
         />
       </View>
-      <View style={styles.storeContentContainer}>
-        <Text>{store.title}</Text>
-        <Text>{store.address}</Text>
-        <Text>{store.phone}</Text>
-      </View>
+      {navigation.state.routeName === "sale" ? <View style={styles.storeContentContainer}>
+        <Text>{store.title}</Text><Text>Sale on {store.saledescription} - {store.sale}% off</Text></View> :
+        <View style={styles.storeContentContainer}>
+          <Text>{store.title}</Text>
+          <Text>{store.address}</Text>
+          <Text>{store.phone}</Text>
+
+        </View>
+      }
     </View>
   </TouchableOpacity>
 );
--- client/js/screens/Sale/SaleContainer.js
+++ client/js/screens/Sale/SaleContainer.js
@@ -1,14 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Sale from './Sale';
-import {withNavigation} from 'react-navigation';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import Loader from '../../components/Loader';
+import { Text } from 'react-native';
 
+const GET_SALE_STORES = gql`
+query {
+  allStores(filter:{sale_gt: 0}){
+    id
+    address
+    categories
+    email
+    hours
+    id
+    images
+    phone
+    sale
+    storeLogo
+    title
+    website
+    saledescription
+  }
+  
+}
+`
 class SaleContainer extends Component {
   static navigationOptions = {
     title: 'Sale',
   };
   render() {
-    return <Sale />;
+    const { navigation } = this.props
+    return (
+      <Query query={GET_SALE_STORES}>
+        {({ loading, error, data }) => {
+          if (loading) return <Loader />
+          if (error) return <Text>{error.message}</Text>;
+          return <Sale stores={data.allStores} navigation={navigation} />;
+        }
+        }
+      </Query>
+    )
   }
 }
 
 export default SaleContainer;
+
