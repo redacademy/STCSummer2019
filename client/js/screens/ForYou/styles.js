3dc4f45bd8fc52bdf9845835df71f9a30b893c18
verasunyy <verasun@live.com>
2019-09-16 10:14:52 -0700

    add top bar for the screen

--- client/js/screens/ForYou/styles.js
+++ client/js/screens/ForYou/styles.js
@@ -1,11 +1,42 @@
 import { StyleSheet } from 'react-native';
-
 const styles = StyleSheet.create({
   allItems: {
     flexDirection: 'row',
     flexWrap: 'wrap',
-    paddingTop: '3%'
+  },
+  sortItems: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 15,
+    paddingLeft: '5%',
+  },
+  allItemsContainer: {
+    paddingTop: '5%',
+    paddingLeft: '3%',
+    paddingRight: '3%',
+  },
+  sortItemsText: {
+    fontWeight: '500',
+    fontSize: 14,
+    letterSpacing: 0.3,
+    paddingLeft: '3%',
+    paddingRight: '3%',
+  },
+  dropdown: {
+    height: 15,
+    width: '15%',
+  },
+  itemFilter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  loader: {
+    justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
   },
 });
+
+
 
 export default styles;
