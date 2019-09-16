4545c9b71b17fa39821bfe23ee726a682dbadb61
Sydney Douglas <sydneydouglas@Jasmines-MacBook-Pro.local>
2019-09-16 10:23:38 -0700

    Clean up brandtab

--- client/js/components/BrandTab/BrandTab.js
+++ client/js/components/BrandTab/BrandTab.js
@@ -5,6 +5,7 @@ import FavesBrandsContext from '../../context/FaveBrandsContext';
 import {Query} from 'react-apollo';
 import styles from './styles';
 import {withNavigation} from 'react-navigation';
+import Loader from '../Loader';
 
 const GET_All_BRANDS = gql`
   {
@@ -24,91 +25,74 @@ const GET_All_BRANDS = gql`
     }
   }
 `;
+const BrandTab = () => {
+  const {navigation} = this.props;
 
-class BrandTab extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  return (
+    <FavesBrandsContext.Consumer>
+      {({faveBrandIds}) => (
+        <Query query={GET_All_BRANDS}>
+          {({loading, error, data}) => {
+            if (loading) return <Loader />;
+            if (error) return <Text>Error :(</Text>;
+            if (data) {
+              const brandsData = data.allBrands;
+              const brands = brandsData.filter(
+                brand => faveBrandIds && faveBrandIds.includes(brand.id),
+              );
 
-  render() {
-    const {navigation} = this.props;
-
-    return (
-      <FavesBrandsContext.Consumer>
-        {({faveBrandIds}) => (
-          <Query query={GET_All_BRANDS}>
-            {({loading, error, data}) => {
-              if (loading) return <Text>Loading :(</Text>;
-              if (error) return <Text>Error :(</Text>;
-              if (data) {
-                const brandsData = data.allBrands;
-                const brands = brandsData.filter(
-                  brand => faveBrandIds && faveBrandIds.includes(brand.id),
-                );
-
-                return (
-                  <View style={styles.container}>
-                    {brands.length > 0 ? (
-                      <ScrollView>
-                        <View style={styles.favItemWrapper}>
-                          {brands.map(brand => (
-                            <View
-                              style={styles.subfavItemWrapper}
-                              key={brand.id}>
-                              <TouchableOpacity
-                                style={styles.imgWrapper}
-                                onPress={() =>
-                                  navigation.navigate('brand', {brand})
-                                }>
-                                <Image
-                                  resizeMode="cover"
-                                  source={{uri: brand.images[0]}}
-                                  style={styles.itemImage}
-                                />
-                              </TouchableOpacity>
-                            </View>
-                          ))}
-                        </View>
-                      </ScrollView>
-                    ) : (
-                      <View style={styles.container}>
-                        <View style={styles.containerImg}>
-                          <Image
-                            style={styles.iconImg}
-                            resizeMode="contain"
-                            source={require('../../assets/icons/azheart.png')}
-                          />
-                        </View>
-                        <Text style={styles.text}>
-                          {' '}
-                          See the brands you want{' '}
-                        </Text>
-                        <Text style={styles.subText}>
-                          {' '}
-                          Curate your own personal brand by favouriting the
-                          brands that represent you.{' '}
-                        </Text>
-                        <TouchableOpacity
-                          onPress={() => navigation.navigate('Stores&Brands')}>
-                          <View style={styles.txtWrapper}>
-                            <Text style={styles.browseTxt}>
-                              {' '}
-                              Browse Brands{' '}
-                            </Text>
+              return (
+                <View style={styles.container}>
+                  {brands.length > 0 ? (
+                    <ScrollView>
+                      <View style={styles.favItemWrapper}>
+                        {brands.map(brand => (
+                          <View style={styles.subfavItemWrapper} key={brand.id}>
+                            <TouchableOpacity
+                              style={styles.imgWrapper}
+                              onPress={() =>
+                                navigation.navigate('brand', {brand})
+                              }>
+                              <Image
+                                resizeMode="cover"
+                                source={{uri: brand.images[0]}}
+                                style={styles.itemImage}
+                              />
+                            </TouchableOpacity>
                           </View>
-                        </TouchableOpacity>
+                        ))}
                       </View>
-                    )}
-                  </View>
-                );
-              }
-            }}
-          </Query>
-        )}
-      </FavesBrandsContext.Consumer>
-    );
-  }
-}
+                    </ScrollView>
+                  ) : (
+                    <View style={styles.container}>
+                      <View style={styles.containerImg}>
+                        <Image
+                          style={styles.iconImg}
+                          resizeMode="contain"
+                          source={require('../../assets/icons/azheart.png')}
+                        />
+                      </View>
+                      <Text style={styles.text}> See the brands you want </Text>
+                      <Text style={styles.subText}>
+                        Curate your own personal brand by favouriting the brands
+                        that represent you.
+                      </Text>
+                      <TouchableOpacity
+                        onPress={() => navigation.navigate('Stores&Brands')}>
+                        <View style={styles.txtWrapper}>
+                          <Text style={styles.browseTxt}> Browse Brands </Text>
+                        </View>
+                      </TouchableOpacity>
+                    </View>
+                  )}
+                </View>
+              );
+            }
+          }}
+        </Query>
+      )}
+    </FavesBrandsContext.Consumer>
+  );
+};
 
 export default withNavigation(BrandTab);
