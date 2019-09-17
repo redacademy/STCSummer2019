3237c87b0e01b027ddf99bcdd551e5825471ba10
Sydney Douglas <sydneydouglas@Jasmines-MacBook-Pro.local>
2019-09-17 16:39:40 -0700

    Add filter feature to brand item page

--- client/js/screens/BrandItems/BrandItems.js
+++ client/js/screens/BrandItems/BrandItems.js
@@ -1,41 +1,8 @@
 import React from 'react';
-import {View, Text, ScrollView, Image} from 'react-native';
-import ItemsList from '../../components/ItemList';
-import styles from './styles';
+import Filter from '../../components/Filter';
 
 const BrandItems = ({navigation, allItems}) => {
-  return (
-    <ScrollView style={styles.allItemsContainer}>
-      <View style={styles.sortItems}>
-        <View style={styles.itemFilter}>
-          <Text style={styles.sortItemsText}>Sort</Text>
-          <Image
-            style={styles.dropdown}
-            source={require('../../assets/icons/dropdown.png')}
-          />
-        </View>
-        <View style={styles.itemFilter}>
-          <Text style={styles.sortItemsText}>Tags</Text>
-          <Image
-            style={styles.dropdown}
-            source={require('../../assets/icons/dropdown.png')}
-          />
-        </View>
-        <View style={styles.itemFilter}>
-          <Text style={styles.sortItemsText}>Filter</Text>
-          <Image
-            style={styles.dropdown}
-            source={require('../../assets/icons/inactivefilter.png')}
-          />
-        </View>
-      </View>
-      <View style={styles.allItems}>
-        {allItems.map(item => (
-          <ItemsList item={item} key={item.id} navigation={navigation} />
-        ))}
-      </View>
-    </ScrollView>
-  );
+  return <Filter items={allItems} navigation={navigation} />;
 };
 
 export default BrandItems;
