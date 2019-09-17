386797ae01a6970b80b5aa8d87871cbb80527265
Yen Yin Hing <yenyinf287@gmail.com>
2019-09-17 13:55:15 -0700

    fix cogwheel style

--- client/js/navigation/styles.js
+++ client/js/navigation/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   image: {
@@ -23,9 +23,9 @@ const styles = StyleSheet.create({
 
   cogwheelIcon: {
     resizeMode: 'contain',
-    height: '25%',
-    width: '25%',
-    marginLeft: '55%',
+    height: '58%',
+    width: '58%',
+    marginLeft: '10%',
   },
   backContainer: {
     height: 50,
